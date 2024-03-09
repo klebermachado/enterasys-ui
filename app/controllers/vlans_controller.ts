1f9db2a05b884e36c1ae20509bae663519c7dd4b
Kleber Machado <nkleber.machado@gmail.com>
2024-03-09 18:32:50 -0300

    create controllers

--- app/controllers/vlans_controller.ts
+++ app/controllers/vlans_controller.ts
@@ -4,14 +4,10 @@ import { inject } from '@adonisjs/core'
 import HtmxService from '../services/htmx_service.js'
 
 @inject()
-export default class SwitchesController {
+export default class VlansController {
   constructor(private hx: HtmxService) {}
 
-  async show() {
-    return this.hx.render('pages/home')
-  }
-
   async create() {
-    return this.hx.render('pages/switches/create')
+    return this.hx.render('pages/vlans/index')
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -9,5 +9,8 @@
 
 import router from '@adonisjs/core/services/router'
 import SwitchesController from '../app/controllers/switches_controller.js'
+import VlansController from '../app/controllers/vlans_controller.js'
 
 router.get('/', [SwitchesController, 'show'])
+router.get('switches', [SwitchesController, 'create'])
+router.get('vlans', [VlansController, 'create'])
