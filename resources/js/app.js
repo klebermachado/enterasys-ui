1f2704b99e5e37684ef77c05ad10acc3addef4b4
Kleber Machado <nkleber.machado@gmail.com>
2024-03-09 22:44:56 -0300

    install Alpine.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,42 @@
 import htmx from 'htmx.org'
+import Alpine from 'alpinejs'
+import { initFlowbite } from 'flowbite';
 
 window.htmx = htmx
+window.Alpine = Alpine
 
 import('htmx.org/dist/ext/path-params')
+Alpine.start()
 
-import { initFlowbite } from 'flowbite';
+
 htmx.onLoad(() => {
 	initFlowbite();
 })
+
+window.waitForAlpine = (cb) => {
+	let count = 0
+	const interval = setInterval(() => {
+		if (window.Alpine) {
+			clearInterval(interval)
+			cb()
+		}
+		if (count > 100) {
+			clearInterval(interval)
+		}
+		count++
+	}, 100)
+}
+
+window.waitForHtmx = (cb) => {
+	let count = 0
+	const interval = setInterval(() => {
+		if (window.htmx) {
+			clearInterval(interval)
+			cb()
+		}
+		if (count > 100) {
+			clearInterval(interval)
+		}
+		count++
+	}, 100)
+}
