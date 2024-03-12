1b0e2120ec4d7c144f85587fa77115b78bc21fb3
Kleber Machado <nkleber.machado@gmail.com>
2024-03-12 11:07:55 -0300

    create navbar to switch

--- app/controllers/switches_controller.ts
+++ app/controllers/switches_controller.ts
@@ -7,7 +7,7 @@ export default class SwitchesController {
   constructor(private hx: HtmxService) {}
 
   async index({ params }: HttpContext) {
-    return this.hx.render('pages/switches/index', { slug: `Switch baum demais ${params.slug}` })
+    return this.hx.render('pages/switches/index', { id: 23 })
   }
 
   async show() {
@@ -16,5 +16,21 @@ export default class SwitchesController {
 
   async create() {
     return this.hx.render('pages/switches/create')
+  }
+
+  async vlansPage({ params }: HttpContext) {
+    return this.hx.render('pages/switches/vlans', { slug: params.slug })
+  }
+
+  async portsPage({ params }: HttpContext) {
+    return this.hx.render('pages/switches/ports', { slug: params.slug })
+  }
+
+  async configPage({ params }: HttpContext) {
+    return this.hx.render('pages/switches/config', { slug: params.slug })
+  }
+
+  async backupPage({ params }: HttpContext) {
+    return this.hx.render('pages/switches/backup', { slug: params.slug })
   }
 }
