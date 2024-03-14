ef7600ae90a981e9ad210d43c0fe489ba6c7463b
Kleber Machado <nkleber.machado@gmail.com>
2024-03-13 22:12:09 -0300

    allow multi level load page

--- app/services/htmx_service.ts
+++ app/services/htmx_service.ts
@@ -5,13 +5,30 @@ import { HttpContext } from '@adonisjs/core/http'
 export default class HtmxService {
   constructor(private ctx: HttpContext) {}
 
-  render(page: string, content?: any, template?: string) {
+  render(page: string | string[], content?: any, template?: string) {
     if (!template) {
       template = 'layouts/app'
     }
     if (this.ctx.request.header('HX-Request')) {
-      return this.ctx.view.render(page, content)
+      if (Array.isArray(page)) {
+        return this.ctx.view.render(page[page.length - 1], content)
+      } else {
+        return this.ctx.view.render(page, content)
+      }
     }
-    return this.ctx.view.render(template, { partialHtmx: page, ...content })
+    if (Array.isArray(page)) {
+      const pages = page
+        .map((p, i) => ({ [`pageLevel${i + 1}`]: p }))
+        .reduce((acc, curr) => ({ ...acc, ...curr }), {})
+      return this.ctx.view.render(template, {
+        ...pages,
+        ...content,
+      })
+    } else {
+      return this.ctx.view.render(template, {
+        pageLevel1: page,
+        ...content,
+      })
+    }
   }
 }
